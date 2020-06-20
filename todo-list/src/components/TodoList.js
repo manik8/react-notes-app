9d61e2559c03f6e58cfba466e1c218ef866e07c3
manik8 <maniksharma2014.ms@gmail.com>
2020-06-20 20:18:58 +0530

    second commit

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import Item from './TodoItem';
-import TodoItem from './TodoItem';
 export default class TodoList extends Component {
     render() {
         return (
             <div>
                 hello from Todolist
-                <TodoItem />
+                <Item />
             </div>
         )
     }
